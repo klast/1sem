dc42a1bc20d61642a0364d996e7ecd5aa73fea56
klast <spele.vova@gmail.com>
2017-10-22 19:14:02 +0500

    1 лаба сделана

--- urakov/1/first.py
+++ urakov/1/first.py
@@ -4,7 +4,7 @@
 
 Это временный скриптовый файл.
 """
-import numpy
+import math
 
 def swap(st, i, j):
     st[i], st[j] = st[j], st[i]
@@ -37,10 +37,38 @@ def prev(current):
     res = swap(res, i, j)
     res[i+1:] = res[:i:-1]
     return "".join(res)
+
+def num_on_str(current):
+    res = 0
+    n = len(current)
+    for i in range(n):
+        k = 0
+        for elem in current[i+1:]:
+            if elem < current[i]:
+                k += 1
+        res += math.factorial(n - 1 - i) * k
+    return res + 1
     
-res = next('abcd')
+def str_on_num(begin, num):
+    num +=1
+    res = []
+    unused = list(begin)
+    n = len(begin)
+    while n > 0:
+        n -= 1
+        k = num // math.factorial(n)
+        res.append(unused[k])
+        unused.pop(k)
+        num %= math.factorial(n)
+    return "".join(res)
+    
+res = next('12354')
 print(res)
 res = prev(res)
+print(res)
+res = num_on_str('12354')
+print(res)
+res = str_on_num('12345', 5)
 print(res)
         
 
---
+++ urakov/2/second.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 22 19:11:05 2017
+
+@author: СпелеВова
+"""
+
