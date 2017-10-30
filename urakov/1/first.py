051d08a579c7d86c053f7ef61ebe4b1071827562
Спеле Владимир <spele.vova@gmail.com>
2017-10-30 21:12:59 +0500

    Первая и вторая лаба по Уракову

--- urakov/1/first.py
+++ urakov/1/first.py
@@ -6,21 +6,17 @@
 """
 import math
 
-def swap(st, i, j):
-    st[i], st[j] = st[j], st[i]
-    return st
-
 def next(current):
     i = len(current) - 2
     res = list(current)
     while i >= 0 and current[i] > current[i + 1]:
         i -= 1
     if i < 0:
-        return []
+        return None
     j = len(current) - 1
     while current[j] < current[i]:
         j -= 1
-    res = swap(res, i, j)    
+    res[i], res[j] = res[j], res[i] 
     res[i+1:] = res[:i:-1]
     return "".join(res)
 
@@ -30,11 +26,11 @@ def prev(current):
     while i >= 0 and current[i] < current[i + 1]:
         i -= 1
     if i < 0:
-        return []
+        return None
     j = len(current) - 1
     while current[j] > current[i]:
         j -= 1
-    res = swap(res, i, j)
+    res[i], res[j] = res[j], res[i] 
     res[i+1:] = res[:i:-1]
     return "".join(res)
 
@@ -47,13 +43,14 @@ def num_on_str(current):
             if elem < current[i]:
                 k += 1
         res += math.factorial(n - 1 - i) * k
-    return res + 1
+    return res
     
 def str_on_num(begin, num):
-    num +=1
     res = []
     unused = list(begin)
     n = len(begin)
+    if num > math.factorial(n) - 1 or num < 0:
+        return None
     while n > 0:
         n -= 1
         k = num // math.factorial(n)
@@ -61,12 +58,42 @@ def str_on_num(begin, num):
         unused.pop(k)
         num %= math.factorial(n)
     return "".join(res)
-    
-res = next('12354')
+
+def print_next(begin):
+    while begin != None:
+        begin = next(begin)
+        print(begin)
+
+def print_prev(begin):
+    while begin != None:
+        begin = prev(begin)
+        print(begin)
+        
+def print_nums(begin, way='FORWARD'):
+    while begin != None:
+        print(num_on_str(begin), begin)
+        if way == 'FORWARD':
+            begin = next(begin)
+        elif way == 'BACKWARD':
+            begin = prev(begin)
+            
+def print_str_on_num(begin, num, way = 'FORWARD'):
+    current = str_on_num(begin, num)
+    print(current, num)
+    while True:
+        current = str_on_num(begin, num)
+        if current == None:
+            return        
+        print(current, num)
+        if way == 'FORWARD':
+            num += 1
+        elif way == 'BACKWARD':
+            num -= 1   
+res = next('abcd')
 print(res)
 res = prev(res)
 print(res)
-res = num_on_str('12354')
+res = num_on_str('bac')
 print(res)
 res = str_on_num('12345', 5)
 print(res)
--- urakov/2/second.py
+++ urakov/2/second.py
@@ -15,35 +15,31 @@ def sort_bubble(lst):
                 res[j], res[j - 1] = res[j - 1], res[j]
     return res
 
-def sort_insert(lst):
-    res = lst.copy()
-    n = len(res)
-    for i in range(1, n):
-        while i > 0 and res[i] < res[i - 1]:
-            res[i], res[i - 1] = res[i - 1], res[i]
-            i -= 1
-    return res
-
 def sort_select(lst):
     res = lst.copy()
-    for i in range(len(res)):
+    n = len(res)
+    for i in range(n):
         i_min = i
-        for j in range(i + 1, len(res)):
+        for j in range(i + 1, n):
             if res[j] < res[i_min]:
                 i_min = j
         res[i_min], res[i] = res[i], res[i_min]
     return res
 
 def sort_merge(lst):
-    res = lst.copy()
-    
+    res = lst.copy()    
     if len(res) > 1:
         mid = len(res) // 2
         left = res[:mid].copy()
-        right = res[mid:].copy()
-        left = sort_merge(left)
-        right = sort_merge(right)
+        right = res[mid:].copy()        
+        if len(left) > 1:
+            print("Beginning to sort left = ",left)
+            left = sort_merge(left)
+        if len(right) > 1:
+            print("Beginning to sort right = ", right)
+            right = sort_merge(right)
         i, j, k = 0, 0, 0
+        print("Beginning to merge ", left ,'and ', right)
         while i < len(left) and j < len(right):
             if left[i] < right[j]:
                 res[k] = left[i]
@@ -52,19 +48,27 @@ def sort_merge(lst):
                 res[k] = right[j]
                 j += 1
             k += 1
+            [ print(res[i],end=" ") for i in range(k)]
+            print("",end = '\n')
+            
         while i < len(left):
             res[k] = left[i]
             i += 1
             k += 1
-        
+            [ print(res[i],end=" ") for i in range(k)]
+            print("",end = '\n')
         while j < len(right):
             res[k] = right[j]
             j += 1
             k += 1
+            [ print(res[i],end=" ") for i in range(k)]
+            print("",end = '\n')
+        if len(left) > 1 or len(right) > 1:
+            print('merged = ', res)
     return res
 
-begin = np.random.randint(1,100,10)
-print(sort_bubble(begin))
-print(sort_insert(begin))
-print(sort_select(begin))
-print(sort_merge(begin))
+begin = np.random.randint(1,100,8)
+print("unsorted = ", begin)
+#print(sort_bubble(begin))
+#print(sort_select(begin))
+print('sorted_list = ', sort_merge(begin))
